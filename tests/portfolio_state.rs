100078821d917164c70c296e482a9748e12ae183
willyrgf <willyr.goncalves@gmail.com>
2022-07-15 11:52:43 -0300

    fix formatting

--- tests/portfolio_state.rs
+++ tests/portfolio_state.rs
@@ -7,12 +7,12 @@ async fn portfolio_state_a_200_for_valid_json_body() {
     let response = {
         let client = reqwest::Client::new();
         let body = r#"
-					{
-						"token_id": "04a370dc-c864-453c-875a-bf00ee839ae7",
-						"rebalancer_label": "label1",
-						"data": {"test": "aaa"}
-					}
-				"#;
+            {
+                "token_id": "04a370dc-c864-453c-875a-bf00ee839ae7",
+                "rebalancer_label": "label1",
+                "data": {"test": "aaa"}
+            }
+        "#;
 
         client
             .post(&format!("{}/portfolio_state", &addr))
@@ -39,32 +39,32 @@ async fn portfolio_state_a_400_for_invalid_json_body() {
     let test_cases = vec![
         BodyAndMessage {
             body: r#"
-				{
-					"token_id": "wrong-uuid-format-here",
-					"rebalancer_label": "label1",
-					"data": {"test": "aaa"}
-				}
-			"#
+                {
+                    "token_id": "wrong-uuid-format-here",
+                    "rebalancer_label": "label1",
+                    "data": {"test": "aaa"}
+                }
+            "#
             .to_string(),
             message: "wrong uuid format in token_id".to_string(),
         },
         BodyAndMessage {
             body: r#"
-				{
-					"token_id": "04a370dc-c864-453c-875a-bf00ee839ae7",
-					"data": {"test": "aaa"}
-				}
-			"#
+                {
+                    "token_id": "04a370dc-c864-453c-875a-bf00ee839ae7",
+                    "data": {"test": "aaa"}
+                }
+            "#
             .to_string(),
             message: "missing rebalancer_label".to_string(),
         },
         BodyAndMessage {
             body: r#"
-				{
-					"token_id": "04a370dc-c864-453c-875a-bf00ee839ae7",
-					"rebalancer_label": "label1",
-				}
-			"#
+                {
+                    "token_id": "04a370dc-c864-453c-875a-bf00ee839ae7",
+                    "rebalancer_label": "label1",
+                }
+            "#
             .to_string(),
             message: "missing data".to_string(),
         },

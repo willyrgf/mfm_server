8cf5c060bd44b4397281905f3160fc7e054e5412
willyrgf <willyr.goncalves@gmail.com>
2022-07-29 11:22:25 -0300

    use instrumentation in integration tests

--- tests/common.rs
+++ tests/common.rs
@@ -1,5 +1,7 @@
 use std::net::TcpListener;
 
+use mfm_server::telemetry::{get_subscriber, init_subscriber};
+use once_cell::sync::Lazy;
 use sqlx::PgPool;
 
 pub struct TestApp {
@@ -7,7 +9,17 @@ pub struct TestApp {
     pub db_pool: PgPool,
 }
 
+const APP_NAME: &str = "integration_test";
+const DEFAULT_LOG_LEVEL: &str = "debug";
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let subscriber = get_subscriber(APP_NAME.into(), DEFAULT_LOG_LEVEL.into(), std::io::stdout);
+    init_subscriber(subscriber);
+});
+
 pub async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind the random local port");
     let db_pool = mfm_server::establish_db_pool().await;
 
